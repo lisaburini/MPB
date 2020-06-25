ce86bdb34fe12daa482bb11cc165c946a6c7eca0
Lisa <lisaburini.98@gmail.com>
2020-06-25 11:13:30 +0200

    Aggiunte pagine modifica profilo/password, profilo

---
+++ MPB/EditPasswordPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MPB
+{
+    public partial class EditPasswordPage : ContentPage
+    {
+        IAuth auth;
+        public EditPasswordPage()
+        {
+            InitializeComponent();
+            auth = DependencyService.Get<IAuth>();
+        }
+
+        private async void Edit(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(oldPassword.Text) || String.IsNullOrEmpty(oldPassword.Text))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Try Again", "OK");
+            }
+            else
+            {
+                string task = await auth.EditPassword(oldPassword.Text, newPassword.Text);
+                if (Equals(task, "ok"))
+                {
+                    App.Current.MainPage = new MainPage();
+                }
+                else if (Equals(task, null))
+                {
+                    await App.Current.MainPage.DisplayAlert("Error", "Try Again", "OK");
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ MPB/EditProfilePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MPB
+{
+
+    public partial class EditProfilePage : ContentPage
+    {
+        IFirestore firestore;
+        public EditProfilePage()
+        {
+            InitializeComponent();
+            firestore = DependencyService.Get<IFirestore>();
+        }
+
+        private async void Edit(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(name.Text) && String.IsNullOrEmpty(lastName.Text))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Try Again", "OK");
+            }
+            else
+            if (String.IsNullOrEmpty(name.Text) || String.IsNullOrEmpty(lastName.Text))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Try Again", "OK");
+            }
+
+            else
+            {
+
+                var token = await firestore.EditData(name.Text, lastName.Text);
+
+                App.Current.MainPage = new MainPage();
+
+            }
+        }
+    }
+
+}
