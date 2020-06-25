2d97d64aeb565a5affca1e39e098d69c66df2ef5
Lisa <lisaburini.98@gmail.com>
2020-06-25 10:53:21 +0200

    pagina profilo, interfaccia e classe per firestore

---
+++ MPB/IFireStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using MPB.Models;
+
+namespace MPB
+{
+    public interface IFirestore
+    {
+        Task<string[]> GetData();
+
+        Task<string> EditData(string name, string lastName);
+
+        Task<string> AddTransaction(string category, float money);
+
+        Task<string> AddTransaction2(string category, float money);
+
+        Task<ObservableCollection<Transaction>> GetTransactions();
+
+        Task<string> TotalSum();
+
+        Task<ObservableCollection<Transaction>> GetSomeTransactions(string category);
+
+        Task<string> PartialSum(string category);
+
+        Task<float> SumEarnings();
+
+        Task<float> SumOutflows();
+
+
+
+    }
+}
---
+++ MPB/ProfilePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MPB
+{
+    public partial class ProfilePage : ContentPage
+    {
+        IFirestore firestore;
+        public ProfilePage()
+        {
+            InitializeComponent();
+            firestore = DependencyService.Get<IFirestore>();
+            showData();
+
+        }
+
+        public async void showData()
+        {
+
+            var values = await firestore.GetData();
+            LabelName.Text = values.GetValue(0).ToString();
+            LabelLastName.Text = values.GetValue(1).ToString();
+            LabelEmail.Text = values.GetValue(2).ToString();
+
+            //var values = firestore.GetData().Result;
+            //LabelName.Text = (string)values.GetValue(0);
+
+        }
+    }
+}
